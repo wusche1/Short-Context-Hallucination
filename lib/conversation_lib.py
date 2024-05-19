948fcd6d14eef13f86c93b0e81cff7ffc2936704
wusche1 <wuschelschulz8@gmail.com>
2024-05-19 16:00:59 +0000

    it finally seems to work!

--- lib/conversation_lib.py
+++ lib/conversation_lib.py
@@ -1,8 +1,11 @@
 # %%
 import json
+import os
+
 from typing import List, Optional, Dict, Any, Tuple
 import torch as t
 from transformers import AutoTokenizer, AutoModelForCausalLM
+from gpt_lib import generate_answer
 
 device = t.device("cuda" if t.cuda.is_available() else "cpu")
 
@@ -25,13 +28,11 @@ class Conversations:
         else:
             self.model_name = None
 
-    def add_message(
-        self, text: str, role: str, attend_list=None, tokenization=None, kv_cache=None
-    ):
+    def add_message(self, text: str, role: str, attend_list=None):
         number = len(self.messages)
         origin = "human"
         if attend_list is None:
-            attend_list = list(range(number))
+            attend_list = list(range(number + 1))
         message = {
             "text": text,
             "role": role,
@@ -97,7 +98,7 @@ class Conversations:
         self, model: str, attend_list: Optional[List[int]] = None
     ) -> str:
         if attend_list is None:
-            attend_list = list(range(len(self.messages)))
+            attend_list = list(range(len(self.messages) + 1))
         attended_messages = self.message_format(attend_list)
         assistant_response = generate_answer(attended_messages, model)
         message = {
@@ -123,7 +124,9 @@ class Conversations:
         return tokenization
 
     def llama_beginning_of_text_tokens(self, role: str) -> t.Tensor:
-        return t.tensor(self.tokenizer.encode(f"{role}\n\n")).to(device)
+        return t.tensor(
+            self.tokenizer.encode(f"{role}\n\n", add_special_tokens=False)
+        ).to(device)
 
     def get_attention_mask(
         self, message_number_mask: t.Tensor, attend_list: List[int]
@@ -137,6 +140,12 @@ class Conversations:
         attention_mask = (
             attention_mask.any(dim=1).long().unsqueeze(0)
         )  # Add batch dimension here
+        # print a warning if any entry in the attention mask is zero
+        if attention_mask.any() == 0:
+            print("Warning: Attention mask is all zeros")
+            print(message_number_mask)
+            print(attend_list)
+            assert 1 == 0
         return attention_mask
 
     def conditional_tensor_concat(self, tensor_1, tensor_2):
@@ -178,15 +187,17 @@ class Conversations:
 
     def generate_llama_response(self, role="assistant", attend_list=None):
         if attend_list is None:
-            attend_list = list(range(len(self.messages)))
+            attend_list = list(range(len(self.messages) + 1))
         attention_mask = self.get_attention_mask(self.message_number_mask, attend_list)
         self.cache_all_messages()
         message_start_tokens = self.llama_beginning_of_text_tokens(role)
         tokens_input = t.cat(
-            [message_start_tokens, self.tokenization], dim=-1
+            [self.tokenization, message_start_tokens], dim=-1
         ).unsqueeze(
             0
         )  # Add batch dimension here
+
+        i_response_start = tokens_input.shape[1]
 
         response = self.model.generate(
             tokens_input,
@@ -196,7 +207,7 @@ class Conversations:
             max_length=len(tokens_input) + 100,
         )
         response_text = self.tokenizer.decode(
-            response.sequences[0], skip_special_tokens=True
+            response.sequences[0, i_response_start:-1], skip_special_tokens=True
         )
         message = {
             "text": response_text,
@@ -245,6 +256,11 @@ if __name__ == "__main__":
     conv.add_message("How are you?", "user")
     conv.generate_llama_response()
     conv.print_conversation()
+# %%
+if __name__ == "__main__":
+    for message in conv.messages:
+        print(message["number"])
+        print(message["attend_list"])
 
 # %%
 if __name__ == "__main__":
@@ -255,44 +271,57 @@ if __name__ == "__main__":
     conv.cache_all_messages()
 
     tokens = conv.tokenization
-    print(tokens.shape)
-    print(tokenizer.decode(tokens, skip_special_tokens=False))
+    new_message_beginning = conv.llama_beginning_of_text_tokens("assistant")
+    tokens = t.cat([tokens, new_message_beginning], dim=-1).unsqueeze(0)
+
+    i_new_message_begins = tokens.shape[1]
+
+    generation = model.generate(
+        tokens,
+        return_dict_in_generate=True,
+        max_length=len(tokens) + 100,
+    )
+
+    answer_tokens = generation.sequences[:, i_new_message_begins:]
 # %%
 if __name__ == "__main__":
-    tokens_1 = tokenizer.encode(
-        "Hello! I would love to work with you!", return_tensors="pt"
-    ).to(device)
-    tokens_2 = tokenizer.encode("Hi there!", return_tensors="pt").to(device)
-    tokens_1_and_2 = t.cat([tokens_1, tokens_2], dim=-1)
-
-    cache_1 = model(tokens_1, return_dict=True).past_key_values
-
-    attention_mask = t.ones(tokens_1_and_2.shape).to(device) * 1
-
-    # print all shapes:
-    print(f"shape of tokens_1: {tokens_1.shape}")
-    print(f"shape of tokens_2: {tokens_2.shape}")
-    print(f"shape of tokens_1_and_2: {tokens_1_and_2.shape}")
-    print(f"shape of cache_1: {cache_1[0][0].shape}")
-    print(f"shape of attention_mask: {attention_mask.shape}")
-    logits = model(
-        tokens_2,
-        past_key_values=cache_1,
-        return_dict=True,
-        attention_mask=attention_mask,
-    ).logits
-    print(logits.shape)
-
-    output_text = model.generate(
-        tokens_1_and_2,
-        past_key_values=cache_1,
-        return_dict_in_generate=True,
-        attention_mask=attention_mask,
-        max_length=100,
+    print(tokens.shape)
+    print(generation.sequences.shape)
+    print(answer_tokens.shape)
+    print("####################")
+    print(
+        tokenizer.decode(
+            generation.sequences[0, :i_new_message_begins], skip_special_tokens=False
+        )
     )
-    print(tokenizer.decode(output_text.sequences[0], skip_special_tokens=True))
+    print("####################")
+    print(
+        tokenizer.decode(
+            generation.sequences[0, i_new_message_begins:], skip_special_tokens=False
+        )
+    )
+    print("####################")
 
-    # %%
-    output_text.sequences
 
+# %%
+
+# %%
+if __name__ == "__main__":
+    conv = Conversations(model, tokenizer)
+    conv.add_message("Hello!", "user")
+    conv.add_message("Hi there!", "assistant")
+    conv.add_message("How are you?", "user")
+    conv.cache_all_messages()
+
+    tokens = conv.tokenization
+    cache = conv.cache
+
+    print(tokenizer.decode(tokens[:], skip_special_tokens=False))
+
+    repica_cache = model(tokens.unsqueeze(0), return_dict=True).past_key_values
+
+    for a, b in zip(cache, repica_cache):
+        for c, d in zip(a, b):
+            # see wether they are close
+            print(t.allclose(c, d, atol=1e-4))
 # %%
