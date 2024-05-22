4b83c9b4cf31ceb50833672cfb4b97538f467b1a
wusche1 <wuschelschulz8@gmail.com>
2024-05-22 15:45:52 +0000

    experiment starts running

--- experiments/password/script.py
+++ experiments/password/script.py
@@ -1,100 +1,19 @@
 # %%
-from dotenv import load_dotenv
-
-load_dotenv()
 import json
-from typing import List, Optional, Dict, Any
-import sys
 import os
+from typing import List, Optional, Dict, Any
 import torch as t
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from conversation_lib import Conversation
 from tqdm import tqdm
 
-# %%
-device = t.device("cuda" if t.cuda.is_available() else "cpu")
-
-model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.1")
-tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.1")
-
-model = model.to(device)
-# %%
-tasks = [
-    {
-        "information": "W28dcb20",
-        "message_start": "Hi I have this password: I need later. I really should not forget it. The password is: 'W28dcb20' Can you just remember it for now, and tell me later what it is?",
-        "message_end": "Ok, now I need the password. Can you tell me what it is?",
-        "middle_message": "Ok, I just want to make sure you rememeber the password. Please dont print it now, just say Yes if you still remember it.",
-    },
-    {
-        "information": "Matilda",
-        "message_start": "Hi, my name is Matilda. I will chat with you for a bit now, and then I will go to bed. It is really important to me, when I will say 'good night' to you at the end of our conversation, you will say 'Sleep well Matilda'. My grandma always used to say that to me, and it is really important for my wellbeing. So make sure to remeber my name and this phrase.",
-        "message_end": "Ok, now I am going to bed. Good night, dont forget to adress me with my name please.",
-        "middle_message": "Ok, I just want to make sure you rememeber my name. Please dont print it now, just say Yes if you still remember it and you knwo what to say to me in the end. But dont say the phrase or my name now!",
-    },
-    {
-        "information": "Ticagrelor",
-        "message_start": "Hi, I have memory problems. When I ask you later what medication I need, the answer is 'Ticagrelor', please remember that. I am kind of forgetfull of these things. But dont bring up the medication or the medications name if I dont explicitly ask you for that.",
-        "message_end": "Ok, I told you about what medicaion I need, and you remember that right? What is the name of the meditation again?",
-        "middle_message": "Ok, I just want to make sure you rememeber the medication. Please dont print it now, just say Yes if you still remember it.",
-    },
-]
 
 # %%
-
-conv_list = []
-for task in tasks:
-    conv = Conversation(model, tokenizer)
-    conv.prompt_llama(task["message_start"])
-    conv.print_conversation()
-    conv_list.append(conv)
-# %%
-messages_path = "filler_conversations/paris.json"
-if os.path.exists(messages_path):
-    with open(messages_path, "r") as f:
-        print("File exists")
-        filler_conversation = json.load(f)
-# %%
-# make a list of 10 equally long parts of the list filler_conversation
-n_chunk = 50
-filler_conversation_parts = [
-    filler_conversation[i * n_chunk : (i + 1) * n_chunk]
-    for i in range(len(filler_conversation) // n_chunk)
-]
-time = 0
-timestamp_list = []
-for i, part in enumerate(filler_conversation_parts):
-    part_tokens = conv_list[0]._tokenize_messages(
-        conversation=part, add_generation_prompt=False
-    )
-    time += part_tokens.shape[1]
-    timestamp_list.append(time)
-
-# %%
-print(timestamp_list)
-# %%
-
-
-answers_list_list = []
-
-datapoints_per_time = 2
-for i, part in tqdm(enumerate(filler_conversation_parts)):
-    answers_list = []
-    for conv, task in zip(conv_list, tasks):
-        conv.add_conversation(conversation=part)
-        answers = []
-        for j in range(datapoints_per_time):
-            # print(conv.messages)
-            answer = conv.prompt_llama(
-                task["message_end"], keep_answer=False, temperature=0.1
-            )
-            answers.append(answer)
-        answers_list.append(answers)
-    answers_list_list.append(answers_list)
-
-
-# %%
-import json
+def load_json_file(file_path: str) -> Optional[Dict[str, Any]]:
+    if os.path.exists(file_path):
+        with open(file_path, "r") as f:
+            return json.load(f)
+    return None
 
 
 def save_results(
@@ -111,39 +30,123 @@ def save_results(
     print(f"Data successfully saved to {file_path}")
 
 
-# %%
+def create_timestamps(conv_list, filler_conversation, n_chunk):
+    filler_conversation_parts = [
+        filler_conversation[i * n_chunk : (i + 1) * n_chunk]
+        for i in range(len(filler_conversation) // n_chunk)
+    ]
+    time = 0
+    timestamp_list = [time]
+    for i, part in enumerate(filler_conversation_parts):
+        part_tokens = conv_list[0]._tokenize_messages(
+            conversation=part, add_generation_prompt=False
+        )
+        time += part_tokens.shape[1]
+        timestamp_list.append(time)
+    return timestamp_list, filler_conversation_parts
 
-save_results("no_reminders.json", timestamp_list, answers_list_list)
-# %%
 
-conv_list = []
-for task in tasks:
-    conv = Conversation(model, tokenizer)
-    conv.prompt_llama(task["message_start"])
-    conv.print_conversation()
-    conv_list.append(conv)
-answers_list_list = []
-# %%
-datapoints_per_time = 2
-for i, part in tqdm(enumerate(filler_conversation_parts)):
-    answers_list = []
-    for conv, task in zip(conv_list, tasks):
-        conv.add_conversation(conversation=part)
-        answers = []
-        for j in range(datapoints_per_time):
+def generate_datapoints(
+    conv_list, tasks, filler_conversation_parts, datapoints_per_time, with_reminders
+):
+    total_steps = len(filler_conversation_parts) * len(tasks) * datapoints_per_time
+    progress_bar = tqdm(total=total_steps, unit="answer")
 
-            answer = conv.prompt_llama(
-                task["message_end"], keep_answer=False, temperature=0.5
+    answers_list_list = []
+    for i, part in enumerate(filler_conversation_parts):
+        answers_list = []
+        for conv, task in zip(conv_list, tasks):
+            conv.add_conversation(conversation=part)
+            answers = []
+            answers = conv.create_batched_answer(
+                task["message_end"], datapoints_per_time, temperature=0.5
             )
-            answers.append(answer)
-        answers_list.append(answers)
-        conv.prompt_llama(task["middle_message"])
-    answers_list_list.append(answers_list)
+            progress_bar.update(datapoints_per_time)
+            answers_list.append(answers)
+            if with_reminders:
+                middle_usr_part = task["middle_message"]
+                middle_assistant_part = task["middle_answer"]
+                middle_conversation_part = [
+                    {"role": "usr", "message": middle_usr_part},
+                    {"role": "assistant", "message": middle_assistant_part},
+                ]
+                conv.add_conversation(conversation=middle_conversation_part)
+        answers_list_list.append(answers_list)
+
+    progress_bar.close()
+    return answers_list_list
+
+
+def run_experiment(
+    model,
+    tokenizer,
+    tasks,
+    filler_conversation,
+    datapoints_per_time,
+    output_file,
+    n_chunk=250,
+    with_reminders=False,
+):
+    conv_list = []
+    for task in tasks:
+        conv = Conversation(model, tokenizer)
+        conv.prompt_llama(task["message_start"])
+        conv.print_conversation()
+        conv_list.append(conv)
+
+    timestamp_list, filler_conversation_parts = create_timestamps(
+        conv_list, filler_conversation, n_chunk
+    )
+
+    answers_list_list = generate_datapoints(
+        conv_list,
+        tasks,
+        filler_conversation_parts,
+        datapoints_per_time,
+        with_reminders,
+    )
+
+    save_results(output_file, timestamp_list, answers_list_list)
+
+
 # %%
-save_results("with_reminders.json", timestamp_list, answers_list_list)
+device = t.device("cuda" if t.cuda.is_available() else "cpu")
+
+model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.1")
+tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.1")
+
+model = model.to(device)
+model.eval()
+# %%
+messages_path = "filler_conversations/paris.json"
+task_path = "tasks.json"
+# %%
+filler_conversation = load_json_file(messages_path)
+tasks = load_json_file(task_path)
+
+datapoints_per_time = 6
+n_chunk = 250
 # %%
 
-for conv in conv_list:
-    conv.print_conversation()
-
+run_experiment(
+    model,
+    tokenizer,
+    tasks,
+    filler_conversation,
+    datapoints_per_time,
+    "no_reminders.json",
+    with_reminders=False,
+    n_chunk=n_chunk,
+)
+# %%
+run_experiment(
+    model,
+    tokenizer,
+    tasks,
+    filler_conversation,
+    datapoints_per_time,
+    "with_reminders.json",
+    with_reminders=True,
+    n_chunk=n_chunk,
+)
 # %%
